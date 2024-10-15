f83a1296985c0d62078389a076f4d0e82705fee4
RanaRoc <81241923+RanaRoc@users.noreply.github.com>
2024-10-16 00:49:57 +0200

    Seperated the mixed and threaded, everything has been tested and works perfectly

--- SAR/src/mixed/impl/Message.java
+++ SAR/src/mixed/impl/Message.java
@@ -1,62 +1,58 @@
-package impl;
+package mixed.impl;
 
 
 public class Message {
-	
-	private byte[] bytes;
-    private int offset;
-    private int length;
-	
+
+	private byte[] bytesArray;
+	private int offset;
+	private int length;
+
 	public Message(byte[] bytes, int offset, int length) {
-		this.bytes = bytes;
+		this.bytesArray = bytes;
 		this.offset = offset;
 		this.length = length;
 	}
-	
+
 	public Message(byte[] bytes) {
-		this.bytes = bytes;
-		this.offset = 0;
-		this.length = bytes.length;
+		this(bytes, 0, bytes.length);
 	}
-	
+
 	public Message(int length) {
-		this.bytes = new byte[length];
+		this.bytesArray = new byte[length];
 		this.offset = 0;
 		this.length = length;
 	}
-	
+
 	public int getOffset() {
-		return this.offset;
+		return offset;
 	}
-	
+
 	public int getLength() {
-		return this.length;
+		return length;
 	}
 
 	public void setOffset(int offset) {
 		this.offset = offset;
 	}
 
-
 	public void setLength(int length) {
 		this.length = length;
 	}
 
 	public byte[] getBytes() {
-		return bytes;
-	}
-	
-	@Override
-	public String toString() {
-		return new String(bytes);
-	}
-	
-	public byte getByte(int i) {
-		return bytes[i];
-	}
-	
-	public void setByte(byte b, int i) {
-		bytes[i] = b;
+		return bytesArray;
 	}
 
+	public byte getByteAt(int index) {
+		return bytesArray[index];
+	}
+
+	public void setByteAt(byte value, int index) {
+		bytesArray[index] = value;
+	}
+
+	@Override
+	public String toString() {
+		return new String(bytesArray);
+	}
 }
